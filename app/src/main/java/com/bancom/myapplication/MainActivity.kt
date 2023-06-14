40f100e3a998d0373b98ab50339eb649cd36e0d8
luisrojashp <luisrojashp@gmail.com>
2023-06-14 09:32:40 -0500

    config project

--- app/src/main/java/com/bancom/myapplication/MainActivity.kt
+++ app/src/main/java/com/bancom/myapplication/MainActivity.kt
@@ -3,28 +3,19 @@ package com.bancom.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.bancom.myapplication.ui.theme.RetoBanconAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RetoBanconAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            //val loginViewModel = hiltViewModel<LoginViewModel>()
+            //LoginScreen(loginViewModel)
         }
     }
 }
@@ -35,12 +26,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RetoBanconAppTheme {
-        Greeting("Android")
-    }
 }
